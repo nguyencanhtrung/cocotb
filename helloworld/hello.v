aea6c56e04a015a1777db5c92cda67f196e6f38b
Trung Nguyen <nguyencanhtrung@me.com>
2023-06-01 15:29:27 +0700

    Remove some folders

--- helloworld/hello.v
+++
@@ -1,5 +0,0 @@
-module hello;
-initial begin;
-    $display("hello learncocotb.com");
-end
-endmodule
--- helloworld/testbench.py
+++
@@ -1,8 +0,0 @@
-# Simple tests for an counter module
-import cocotb
-from cocotb.triggers import RisingEdge
-
-
-@cocotb.test()
-async def hello_world(dut):
-    pass
